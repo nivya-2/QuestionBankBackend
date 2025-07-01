4369dcbed33696206aa4cf51c368b4d3dda34374
nivya-2 <you@example.com>
2025-07-01 09:58:11 +0530

    feat: implement API to add interview

--- QuestionBank.Api/Controllers/QuestionBankController.cs
+++ QuestionBank.Api/Controllers/QuestionBankController.cs
@@ -22,4 +22,20 @@ public class QuestionBankController : BaseController
         var result = await Mediator.Send(new GetAllInterviewsQuery());
         return Ok(result);
     }
+
+    /// <summary>
+    /// Adds a new interview.
+    /// </summary>
+    /// <param name="command">The command containing interview details to create.</param>
+    /// <returns>
+    /// An <see cref="IActionResult"/> indicating the result of the operation, including an HTTP 201 Created status on success.
+    /// </returns>
+    [HttpPost("interviews")]
+    [ProducesResponseType(typeof(int), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> AddInterview([FromBody] AddInterviewCommand command)
+    {
+        var result = await Mediator.Send(command);
+        return Ok(result);
+    }
+
 }
---
+++ QuestionBank.Application/Features/InterviewManagement/AddInterviewCommand.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using QuestionBank.Application.Contracts.Persistence;
+using QuestionBank.Domain.Entities;
+using static QuestionBank.Shared.QuestionBankEnums;
+
+namespace QuestionBank.Application.Features.InterviewManagement;
+
+public class AddInterviewCommand : IRequest<int>
+{
+    public string Role { get; set; }
+    public string InterviewStatus { get; set; }
+    public float Experience { get; set; }
+    public List<int> SkillIds { get; set; } = new();
+
+}
+public class AddInterviewCommandHandler : IRequestHandler<AddInterviewCommand, int>
+{
+    private readonly IQuestionBankDbContext _questionBankDbContext;
+
+    public AddInterviewCommandHandler(IQuestionBankDbContext questionBankDbContext)
+    {
+        _questionBankDbContext = questionBankDbContext;
+    }
+    
+    public async Task<int> Handle(AddInterviewCommand request, CancellationToken cancellationToken)
+    {
+        if (!Enum.TryParse(request.InterviewStatus, out InterviewStatus status))
+        {
+            throw new ArgumentException($"Invalid interview status: {request.InterviewStatus}");
+        }
+
+        var validSkillIds = await _questionBankDbContext.Skills
+        .Where(s => request.SkillIds.Contains(s.Id))
+        .Select(s => s.Id)
+        .ToListAsync(cancellationToken);
+
+        var invalidSkillIds = request.SkillIds.Except(validSkillIds).ToList();
+        if (invalidSkillIds.Any())
+        {
+            throw new ArgumentException($"Invalid skill IDs: {string.Join(", ", invalidSkillIds)}");
+        }
+
+        var interview = new Interview
+        {
+            Role = request.Role,
+            Status = status,
+            Experience = request.Experience,
+            InterviewSkills = validSkillIds.Select(skillId => new InterviewSkill
+            {
+                SkillId = skillId
+            }).ToList()
+
+        };
+
+        _questionBankDbContext.Interviews.Add(interview);
+            
+        await _questionBankDbContext.SaveChangesAsync(cancellationToken);
+        return interview.Id;
+    }
+}
