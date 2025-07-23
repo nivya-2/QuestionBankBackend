c6db1dd12638c2f30b883e011cda377bfb0a4cab
Dhunan <midhun2017.com>
2025-07-23 11:52:41 +0530

    code formatting

--- QuestionBank.Application/Features/QuestionManagement/GetQuestionsForInterviewQuery .cs
+++ QuestionBank.Application/Features/QuestionManagement/GetQuestionsForInterviewQuery .cs
@@ -57,17 +57,20 @@ public class GetQuestionsForInterviewQueryHandler : IRequestHandler<GetQuestions
         // 5. Return the list of QuestionUpdateDto.
         #endregion
         var interview = await _dbContext.Interviews
-                        .AsQueryable()
                         .Include(i => i.Questions)
                         .FirstOrDefaultAsync(i => i.Id == request.InterviewId, cancellationToken);
+
         if (interview is null)
             throw new KeyNotFoundException($"Interview with ID {request.InterviewId} not found.");
-        var interviewQuestions = interview.Questions.Select(
-            q => new QuestionUpdateDto() {
-                Id =q.Id,
-                Question = q.Question,
-            }
-            ).ToList();
+
+        var interviewQuestions = interview.Questions
+            .Select(q => new QuestionUpdateDto
+            {
+                Id = q.Id,
+                Question = q.Question
+            })
+            .ToList();
+
         return interviewQuestions;
     }
 }
